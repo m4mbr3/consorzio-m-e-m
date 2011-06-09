626906ca934bd4d6b43b185d4477b575605f424f
elio.ermini@gmail.com <elio.ermini@gmail.com@4c3a1adb-739a-2ad4-7264-60b61b3cf5cb>
2011-06-09 21:23:57 +0000

    Here There are the Files and Folders...

---
+++ trunk/ConsorzioMEM/src/consorzio/client/MainEntryPoint.java
@@ -0,0 +1,13 @@
+package consorzio.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class MainEntryPoint implements EntryPoint {
+
+    public void onModuleLoad() {
+        RootPanel.get().add(new Main());
+
+    }
+
+}
